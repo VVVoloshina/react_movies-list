eb82af459ca74a36576c02f196cf58c52ad53f43
Valeria0304 <lera_voloshina03@ukr.net>
2021-07-19 16:27:22 +0300

    Updates

---
+++ precache-manifest.d18ef21af5936296fc6bf6895f346cb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c347d6e61698a7aa5ef235526f5aacc",
+    "url": "./index.html"
+  },
+  {
+    "revision": "47726790c56741d144b5",
+    "url": "./static/css/2.a1df12c0.chunk.css"
+  },
+  {
+    "revision": "4f806c59a7298890c846",
+    "url": "./static/css/main.5b5eb71e.chunk.css"
+  },
+  {
+    "revision": "47726790c56741d144b5",
+    "url": "./static/js/2.cd70d490.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cd70d490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f806c59a7298890c846",
+    "url": "./static/js/main.c76e31d9.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
